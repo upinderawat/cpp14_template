13601d4fed6caebe2512a570b7d1dd3ef4930f20
Upinder Singh <upinderawat@gmail.com>
2019-08-01 20:18:13 +0530

    Beginner Template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define repi(i,a,b) for(int i=a; i<=b; i++) 
+#define repd(i,a,b) for(int i=a; i>=b; i--)
+
+const int INF = ~(1<<31);
+typedef long long int ll;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	return 0;
+}
